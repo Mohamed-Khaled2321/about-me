960e9f3aef910178c1789f659333af14023df945
Mohamed-Khaled2321 <98709232+Mohamed-Khaled2321@users.noreply.github.com>
2023-05-14 00:29:34 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,88 @@
+//window.alert('mohamed')
+
+/*var userAge;
+userAge= Number( window.prompt('enter age'));
+console.log( userAge)*/
+
+2
+
+/*var num = prompt('enter a valid number')
+
+if( num % 3 ==0 && num % 4 ==0 ){
+    console.log('Yes')
+}else{
+    console.log('No')
+}*/
+
+3
+
+
+
+
+
+4
+/*var num = prompt('enter a valid number')
+
+if( num > 0){
+    console.log('positive')
+}else if ( num < 0){
+    console.log('Negative')
+}
+else{
+    console.log('zero')
+}*/
+
+
+6
+/*var num = prompt('enter a number')
+
+if( num % 2 ==0){
+    console.log('even')
+}else if (num % 2 == 1){
+    console.log('odd')
+}*/
+
+
+8
+/*var char = prompt('enter a char')
+
+if( char=='a'  || char=='e' || char=='i' || char=='o' || char=='u' ){
+    console.log('vowel')
+}else{
+    console.log('constant')
+}*/
+
+
+
+9
+for(var i = 1; i<=5; i++ ){
+    console.log(i)
+}
+
+
+
+
+
+
+
+
+14
+//var scour = prompt('enter a scour')
+
+// if( scour >=90){
+//     console.log('A')
+// }else if ( scour >=80){
+//     console.log('B')
+// }
+// else if ( scour >=70){
+//     console.log('C')
+// }
+// else if ( scour >=60){
+//     console.log('D')
+// }
+// else if ( scour >=40){
+//     console.log('E')
+// }
+// else if ( scour <40){
+//     console.log('F')
+// }
